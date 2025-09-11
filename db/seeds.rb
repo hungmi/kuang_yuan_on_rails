97e994e57efdf22cafc611926da1fd225d0ce80e
hungmi <hungmihungmi@gmail.com>
2025-09-11 11:29:15 +0800

    add some initial seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,13 @@ end
 		description: "這裡是專案描述。Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error."
 	})
 end
+
+TabsTitle.create({
+  title: "歡迎光臨"
+})
+
+Admin.create({
+  email: "admin@email.com",
+  password: "admin1234",
+  password_confirmation: "admin1234"
+})
